70c4a1bcfd70366cfd6b41e676106d58d4d2ad64
xxxxxxxxxxxxl <362535698@qq.com>
2021-04-13 23:03:02 +0800

    vv

--- sanyuan/JS-深入数组/手写map方法/1.js
+++ sanyuan/JS-深入数组/手写map方法/1.js
@@ -7,4 +7,17 @@ Array.prototype.map=function(callbackFn,thisArg){
     if(Object.prototype.toString.call(callbackFn) != "[object Function]"){
         throw new TypeError(callbackFn+'不是一个函数')
     }
+    //草案中提到要先装换为对象
+    let O = Object(this)
+    // console.log(O);//Object(0) []
+    let T = thisArg
+    // console.log(T);
+
+
+    let len= O.length >>> 0;
+    let A = new Array(len)
+    for(let k=0;k<len;k++){
+        
+    }
 }
+Array.prototype.map((a,b)=>{a+b},[1,2,3])
