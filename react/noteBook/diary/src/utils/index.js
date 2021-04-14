4ac74bb56712d80dd486603ab50fa3aee93a733d
xxxxxxxxxxxxl <362535698@qq.com>
2021-04-14 21:17:29 +0800

    vv

---
+++ react/noteBook/diary/src/utils/index.js
@@ -0,0 +1,14 @@
+//参数的截取
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r != null) {
+        return  unescape(r[2]); 
+    } else{
+        return null
+    };
+  }
+  
+  module.exports = {
+    getQueryString
+  }
