c281076ea137417ca87f22e1b666bd38c16be733
xxxxxxxxxxxxl <362535698@qq.com>
2021-04-22 23:37:53 +0800

    vv

---
+++ vue/blog/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <Navbar ></Navbar>
+  </div>
+  
+
+  <div class="page">
+    <router-view></router-view>
+  </div>
+  
+  
+</template>
+
+<script >
+import Navbar from './components/navbar/index.vue'
+
+export default {
+  components:{
+    Navbar
+  }
+}
+
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+.header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+  background-color: transparent;
+}
+</style>
---
+++ vue/blog/src/router/index.js
@@ -0,0 +1,26 @@
+import {createRouter,createWebHistory} from 'vue-router'
+import Home from '../views/home/index.vue'
+
+const name='小陈的博客'
+
+const routes=[
+    {
+        path:'/',
+        name:Home,
+        component:Home,
+        meta:{
+            title:'首页-'+name
+        }
+    }
+]
+
+const router=createRouter({
+    history:createWebHistory(),
+    scrollBehavior(){
+        return {top:0}
+    },
+    routes
+})
+
+
+export default router
